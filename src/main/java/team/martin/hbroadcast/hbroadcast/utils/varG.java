8bcb4c8a2f20b6656aae062f3a036589ffa09d77
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-04-01 17:02:57 -0300

    Adicionado novas variáveis globais.

--- src/main/java/team/martin/hbroadcast/hbroadcast/utils/varG.java
+++ src/main/java/team/martin/hbroadcast/hbroadcast/utils/varG.java
@@ -4,9 +4,11 @@ import org.bukkit.ChatColor;
 
 public class varG {
 
-    public static String version = "1.0";
+    public static String version = "1.1";
     public static String prefix = ChatColor.RED + "[hBroadcast] ";
     public static String permissionadmin = "hbroadcast.admin";
     public static String permissionuser = "hbroadcast.user";
+    public static String semPermissao = "Você não possui permissão para executar esse comando";
+    public static String comandoIncompleto = "Você precisa digitar algo para anunciar.";
 
 }
