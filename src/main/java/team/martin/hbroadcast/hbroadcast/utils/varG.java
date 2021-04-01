136a33642a8df32ecad63726fe5684ae0eecf731
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-04-01 20:19:36 -0300

    Adicionado a config.yml

--- src/main/java/team/martin/hbroadcast/hbroadcast/utils/varG.java
+++ src/main/java/team/martin/hbroadcast/hbroadcast/utils/varG.java
@@ -1,14 +1,16 @@
 package team.martin.hbroadcast.hbroadcast.utils;
 
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import team.martin.hbroadcast.hbroadcast.hBroadcast;
 
 public class varG {
 
-    public static String version = "1.1";
-    public static String prefix = ChatColor.RED + "[hBroadcast] ";
-    public static String permissionadmin = "hbroadcast.admin";
-    public static String permissionuser = "hbroadcast.user";
-    public static String semPermissao = "Você não possui permissão para executar esse comando";
-    public static String comandoIncompleto = "Você precisa digitar algo para anunciar.";
+    //Var
+    public static String version = "1.2";
+    public static String prefix = hBroadcast.instancia.getConfig().getString("prefix") + " ";
+    public static String permissionadmin = hBroadcast.instancia.getConfig().getString("PermissaoAdmin");
+    public static String semPermissao = hBroadcast.instancia.getConfig().getString("FraseSemPermissao");
+    public static String comandoIncompleto = hBroadcast.instancia.getConfig().getString("FraseComandoIncompleto");
 
 }
