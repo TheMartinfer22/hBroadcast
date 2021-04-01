42f3a9418aee357b43e171b06580d9c98f68fa51
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-04-01 16:34:31 -0300

    Limpeza de código e adição de comentários.

--- src/main/java/team/martin/hbroadcast/hbroadcast/hBroadcast.java
+++ src/main/java/team/martin/hbroadcast/hbroadcast/hBroadcast.java
@@ -8,22 +8,19 @@ public final class hBroadcast extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        //Frases quabndo o plugin iniciar.
         getLogger().info("");
         getLogger().info("O plugin hBroadcast foi habilitado com sucesso.");
         getLogger().info("Você está utilizando a versão " + varG.version);
         getLogger().info("");
 
+        //Área de importação de comandos.
         getCommand("anunciar").setExecutor(new ComandoAnunciar());
-
-
-
-
-
-
     }
 
     @Override
     public void onDisable() {
-
+        getLogger().info("O plugin foi desligado corretamente.");
+        getLogger().info("Créditos: TheMartinfer22");
     }
 }
