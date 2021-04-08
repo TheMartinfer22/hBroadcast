e38ec797dbb8a06b69c159d242d4414ea0df20cb
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-04-08 11:56:16 -0300

    Adicionado TODO e comentários.

--- src/main/java/team/martin/hbroadcast/hbroadcast/utils/Var.java
+++ src/main/java/team/martin/hbroadcast/hbroadcast/utils/Var.java
@@ -1,16 +1,18 @@
 package team.martin.hbroadcast.hbroadcast.utils;
 
-import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.FileConfiguration;
 import team.martin.hbroadcast.hbroadcast.hBroadcast;
 
-public class varG {
-
-    //Var
-    public static String version = "1.2";
+public class Var {
+    //Variáveis Globais
+    public static String version = "1.2"; //TODO, Aplicar versão globalmente.
     public static String prefix = hBroadcast.instancia.getConfig().getString("prefix") + " ";
     public static String permissionadmin = hBroadcast.instancia.getConfig().getString("PermissaoAdmin");
     public static String semPermissao = hBroadcast.instancia.getConfig().getString("FraseSemPermissao");
     public static String comandoIncompleto = hBroadcast.instancia.getConfig().getString("FraseComandoIncompleto");
 
+    /*
+
+    Estou utilizando o método getConfig para pegar alguma String(Dado) da configuração padrão config.yml.
+
+     */
 }
