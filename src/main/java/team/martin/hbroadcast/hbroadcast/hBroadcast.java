724c7b01022d5be61cf89d22616880e7f3d74078
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-04-08 11:57:25 -0300

    Adicionado TODO, comentários e alterado para Objeto as chamadas de comandos (IDE Me sugestinou que era o melhor método).

--- src/main/java/team/martin/hbroadcast/hbroadcast/hBroadcast.java
+++ src/main/java/team/martin/hbroadcast/hbroadcast/hBroadcast.java
@@ -5,32 +5,41 @@ import org.bukkit.plugin.java.JavaPlugin;
 import team.martin.hbroadcast.hbroadcast.comandos.ComandoAnunciar;
 import team.martin.hbroadcast.hbroadcast.comandos.ComandoAnunciarRaw;
 import team.martin.hbroadcast.hbroadcast.comandos.ComandoReload;
-import team.martin.hbroadcast.hbroadcast.utils.varG;
+import team.martin.hbroadcast.hbroadcast.utils.Var;
+
+import java.util.Objects;
 
 public final class hBroadcast extends JavaPlugin {
     public static hBroadcast instancia;
+
+    //Definindo config uma variável e igualando para
     FileConfiguration config = getConfig();
+
     @Override
     public void onEnable() {
+
+        //Definir instância para a config handler ler
         instancia = this;
-        //Frases quabndo o plugin iniciar.
+
+        //Frases quando o plugin iniciar.
         getLogger().info("");
         getLogger().info("O plugin hBroadcast foi habilitado com sucesso.");
-        getLogger().info("Você está utilizando a versão " + varG.version);
+        getLogger().info("Você está utilizando a versão " + Var.version);
         getLogger().info("");
 
         //Área de importação de comandos.
-        getCommand("anunciar").setExecutor(new ComandoAnunciar());
-        getCommand("anunciarraw").setExecutor(new ComandoAnunciarRaw());
-        getCommand("hbroadcastreload").setExecutor(new ComandoReload());
+        Objects.requireNonNull(getCommand("anunciar")).setExecutor(new ComandoAnunciar());
+        Objects.requireNonNull(getCommand("anunciarraw")).setExecutor(new ComandoAnunciarRaw());
+        Objects.requireNonNull(getCommand("hbroadcastreload")).setExecutor(new ComandoReload());
 
         //Carregar as configurações
+        //O erro de reload é que eu utilizei o SaveResourse então está apenas salvando um arquivo que está na memória, TODO Alterar para retirar o bug. Acrescentar saveConfig().
         saveResource("config.yml", false);
     }
-
     @Override
     public void onDisable() {
         getLogger().info("O plugin foi desligado corretamente.");
         getLogger().info("Créditos: TheMartinfer22");
     }
+
 }
