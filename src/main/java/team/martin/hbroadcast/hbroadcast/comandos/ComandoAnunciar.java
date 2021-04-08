d676b9c020bed61210b90d731412e33e8ba014d1
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-04-08 11:58:26 -0300

    Atualização de VarG para Var, assim facilitar a leitura.

--- src/main/java/team/martin/hbroadcast/hbroadcast/comandos/ComandoAnunciar.java
+++ src/main/java/team/martin/hbroadcast/hbroadcast/comandos/ComandoAnunciar.java
@@ -4,14 +4,14 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import team.martin.hbroadcast.hbroadcast.utils.varG;
+import team.martin.hbroadcast.hbroadcast.utils.Var;
 
 public class ComandoAnunciar implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
-        if (sender.hasPermission(varG.permissionadmin)){
+        if (sender.hasPermission(Var.permissionadmin)){
             if (args.length == 0){
-                sender.sendMessage(varG.prefix + varG.comandoIncompleto);
+                sender.sendMessage(Var.prefix + Var.comandoIncompleto);
             } else {
                 StringBuilder mensagem = new StringBuilder("");
                 for (String fazParte : args){
@@ -19,10 +19,10 @@ public class ComandoAnunciar implements CommandExecutor {
                     mensagem.append(" ");
                     mensagem.append(fazParte);
                 }
-                Bukkit.getServer().broadcastMessage(varG.prefix + mensagem.toString());
+                Bukkit.getServer().broadcastMessage(Var.prefix + mensagem.toString());
             }
             }else {
-            sender.sendMessage(varG.semPermissao);
+            sender.sendMessage(Var.semPermissao);
         }
         return false;
         };
--- src/main/java/team/martin/hbroadcast/hbroadcast/comandos/ComandoAnunciarRaw.java
+++ src/main/java/team/martin/hbroadcast/hbroadcast/comandos/ComandoAnunciarRaw.java
@@ -4,14 +4,14 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import team.martin.hbroadcast.hbroadcast.utils.varG;
+import team.martin.hbroadcast.hbroadcast.utils.Var;
 
 public class ComandoAnunciarRaw implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
-        if (sender.hasPermission(varG.permissionadmin)){
+        if (sender.hasPermission(Var.permissionadmin)){
             if (args.length == 0){
-                sender.sendMessage(varG.prefix + varG.comandoIncompleto);
+                sender.sendMessage(Var.prefix + Var.comandoIncompleto);
             }else {
                 StringBuilder mensagem = new StringBuilder("");
                 //Para os argumentos de args que estiverem com espaçamento, eles vão fazer parte da mesma frase.
@@ -23,7 +23,7 @@ public class ComandoAnunciarRaw implements CommandExecutor {
                 Bukkit.getServer().broadcastMessage(mensagem.toString());
             }
             }else {
-            sender.sendMessage(varG.semPermissao);
+            sender.sendMessage(Var.semPermissao);
         }
 
 
