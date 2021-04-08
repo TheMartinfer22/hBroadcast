4f2e3ca37b4969f684871193265f0996d5469886
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-04-08 11:57:43 -0300

    Adicionado comentário.

--- src/main/java/team/martin/hbroadcast/hbroadcast/comandos/ComandoReload.java
+++ src/main/java/team/martin/hbroadcast/hbroadcast/comandos/ComandoReload.java
@@ -5,16 +5,15 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import team.martin.hbroadcast.hbroadcast.hBroadcast;
-import team.martin.hbroadcast.hbroadcast.utils.varG;
+import team.martin.hbroadcast.hbroadcast.utils.Var;
 
 public class ComandoReload implements CommandExecutor {
     @Override
+    //o erro já está documentado na linha #35 do hbroadcast.
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
-
-
-        if (sender.hasPermission(varG.permissionadmin)) {
+        if (sender.hasPermission(Var.permissionadmin)) {
             sender.sendMessage(ChatColor.RED + "Esse comando está sob manutenção, reinicie o servidor para fazer efeito.");
-            hBroadcast.instancia.saveConfig();
+            hBroadcast.instancia.reloadConfig();
         }
         return false;
     }
