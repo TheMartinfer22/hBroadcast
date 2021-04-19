c30bb03423ff63a33de6f2b5f8528823e1f8a9f6
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-04-18 22:55:32 -0300

    Criação de uma classe para receber instruções ao ligar e desligar o servidor.

---
+++ src/main/java/team/martin/hbroadcast/utils/LogUtils.java
@@ -0,0 +1,26 @@
+package team.martin.hbroadcast.utils;
+
+import org.bukkit.ChatColor;
+import team.martin.hbroadcast.comandos.ComandoAnunciar;
+import team.martin.hbroadcast.hBroadcast;
+
+public class LogUtils {
+
+    public void getInit(){
+        System.out.println();
+        System.out.println(ChatColor.GREEN + "★ [hBroadcast] O plugin hBroadcast foi carregado com sucesso :D");
+        System.out.println(ChatColor.GREEN + "★ [hBroadcast] Você está utilizando a versão " + hBroadcast.getInstance().getDescription().getVersion());
+        System.out.println();
+    }
+
+    public void getComandos(){
+        hBroadcast.getInstance().getCommand("anunciar").setExecutor(new ComandoAnunciar());
+    }
+
+    public void getExit(){
+        System.out.println();
+        System.out.println(ChatColor.RED + "★ [hBroadcast] O plugin hBroadcast foi descarregado com sucesso");
+        System.out.println(ChatColor.RED + "★ [hBroadcast] Até breve!");
+        System.out.println();
+    }
+}
