9cd6e7b08da59df9c8a16b329a190b0e25e9dc51
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-04-18 22:54:43 -0300

    Criação de instâncias para limpar o código.

---
+++ src/main/java/team/martin/hbroadcast/hBroadcast.java
@@ -0,0 +1,26 @@
+package team.martin.hbroadcast;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import team.martin.hbroadcast.utils.LogUtils;
+
+public class hBroadcast extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        LogUtils log = new LogUtils();
+        log.getInit();
+        log.getComandos();
+
+        saveDefaultConfig();
+    }
+    @Override
+    public void onDisable() {
+        LogUtils log = new LogUtils();
+
+        log.getExit();
+    }
+
+    public static hBroadcast getInstance() {
+        return getPlugin(hBroadcast.class);
+    }
+}
