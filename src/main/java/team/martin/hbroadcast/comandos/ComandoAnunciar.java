ed00dde73a21aea80362b7313b8d5bbd239da853
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-04-24 01:54:13 -0300

    Novo recurso de pedir ajuda ao servidor via broadcast. (Cuidado, você dará sua localização)

--- src/main/java/team/martin/hbroadcast/comandos/ComandoAnunciar.java
+++ src/main/java/team/martin/hbroadcast/comandos/ComandoAnunciar.java
@@ -7,51 +7,49 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import team.martin.hbroadcast.utils.ComandosUtils;
 
+import java.util.Objects;
+
 public class ComandoAnunciar implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         ComandosUtils anunciar = new ComandosUtils();
 
-        //Filtrar quem pode acessar o comando Anunciar
-        if (!sender.hasPermission(anunciar.getPermAdministrador()) || !sender.hasPermission(anunciar.getPermStreamer()) || !sender.hasPermission(anunciar.getPermVIP())){
-            sender.sendMessage(anunciar.getCmdSemPermissao());
-            return false;
-        }
-
-        if (args.length > 1){
-            if ((args[0].equalsIgnoreCase(anunciar.getPrefixSubComandoNormal())) && (sender.hasPermission(anunciar.getPermAdministrador()))){
+        if (args.length > 0) {
+            if ((args[0].equalsIgnoreCase(anunciar.getPrefixSubComandoNormal())) && (sender.hasPermission(anunciar.getPermAdministrador()))) {
                 Bukkit.broadcastMessage(String.join(" ", args)
                         .replace(args[0], anunciar.getPrefixBroadcast())
                         .replace("&", "§"));
                 return true;
             }
-
-            if ((args[0].equalsIgnoreCase(anunciar.getPrefixSubComandoRaw())) && (sender.hasPermission(anunciar.getPermAdministrador()))){
+            if ((args[0].equalsIgnoreCase(anunciar.getPrefixSubComandoRaw())) && (sender.hasPermission(anunciar.getPermAdministrador()))) {
                 Bukkit.broadcastMessage(String.join(" ", args)
-                        .replace(args[0] + " ",anunciar.getPrefixRaw())
+                        .replace(args[0] + " ", anunciar.getPrefixRaw())
                         .replace("&", "§"));
                 return true;
             }
-
-            //Live
-            if ((args[0].equalsIgnoreCase(anunciar.getPrefixSubComandoLive())) && (sender.hasPermission(anunciar.getPermAdministrador()))){
+            if ((args[0].equalsIgnoreCase(anunciar.getPrefixSubComandoLive())) && (sender.hasPermission(anunciar.getPermAdministrador()))) {
                 Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "[Live] " + ChatColor.LIGHT_PURPLE + "O jogador " + sender.getName() + " está ao vivo, venha conferir clicando aqui."
                         .replace(args[0] + " ", "")
                         .replace("&", "§"));
                 return true;
             }
 
+            if ((args[0].equalsIgnoreCase(anunciar.getPrefixSubComandoPedirAjuda())) && (sender.hasPermission(anunciar.getPermUser()))) {
+                Bukkit.broadcastMessage("");
+                Bukkit.broadcastMessage("§c§l[!] §cAlguém está criando um pedido de emergência... Capturamos os dados!");
+                Bukkit.broadcastMessage("§c§l[!] " + ChatColor.RED + anunciar.getJogadorLocalizationThis(sender));
+                Bukkit.broadcastMessage("");
+                return true;
+            }
+        }
+        if ((sender.hasPermission(anunciar.getPermUser()) || (sender.hasPermission(anunciar.getPermVIP())) || (sender.hasPermission(anunciar.getPermStreamer()) || (sender.hasPermission(anunciar.getPermAdministrador()))))){
+            sender.sendMessage(anunciar.getCmdIncompleto());
+            return false;
         }
 
-//        if ((args[0].equalsIgnoreCase("reload")) && (sender.hasPermission(anunciar.getPermAdministrador()))){
-//            hBroadcast.getInstance().saveConfig();
-//            hBroadcast.getInstance().reloadConfig();
-//            sender.sendMessage("Você recarregou as configurações do plugin com sucesso!");
-//            return true;
-//        }
+        sender.sendMessage(anunciar.getCmdSemPermissao());
 
-        sender.sendMessage(anunciar.getCmdIncompleto());
         return false;
 
     }
