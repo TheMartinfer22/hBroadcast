b8782f0869e61bdcf463f927af0737204fbd04c4
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-04-25 20:25:55 -0300

    Conversão do ComandoAnunciar para herdar a classe de configurações.

--- src/main/java/team/martin/hbroadcast/comandos/ComandoAnunciar.java
+++ src/main/java/team/martin/hbroadcast/comandos/ComandoAnunciar.java
@@ -7,48 +7,44 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import team.martin.hbroadcast.utils.ComandosUtils;
 
-import java.util.Objects;
-
-public class ComandoAnunciar implements CommandExecutor {
+public class ComandoAnunciar extends ComandosUtils implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-        ComandosUtils anunciar = new ComandosUtils();
-
         if (args.length > 0) {
-            if ((args[0].equalsIgnoreCase(anunciar.getPrefixSubComandoNormal())) && (sender.hasPermission(anunciar.getPermAdministrador()))) {
+            if ((args[0].equalsIgnoreCase(getPrefixSubComandoNormal())) && (sender.hasPermission(getPermAdministrador()))) {
                 Bukkit.broadcastMessage(String.join(" ", args)
-                        .replace(args[0], anunciar.getPrefixBroadcast())
+                        .replace(args[0], getPrefixBroadcast())
                         .replace("&", "§"));
                 return true;
             }
-            if ((args[0].equalsIgnoreCase(anunciar.getPrefixSubComandoRaw())) && (sender.hasPermission(anunciar.getPermAdministrador()))) {
+            if ((args[0].equalsIgnoreCase(getPrefixSubComandoRaw())) && (sender.hasPermission(getPermAdministrador()))) {
                 Bukkit.broadcastMessage(String.join(" ", args)
-                        .replace(args[0] + " ", anunciar.getPrefixRaw())
+                        .replace(args[0] + " ", getPrefixRaw())
                         .replace("&", "§"));
                 return true;
             }
-            if ((args[0].equalsIgnoreCase(anunciar.getPrefixSubComandoLive())) && (sender.hasPermission(anunciar.getPermAdministrador()))) {
+            if ((args[0].equalsIgnoreCase(getPrefixSubComandoLive())) && (sender.hasPermission(getPermAdministrador()))) {
                 Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "[Live] " + ChatColor.LIGHT_PURPLE + "O jogador " + sender.getName() + " está ao vivo, venha conferir: "
                         .replace(args[0] + " ", "")
                         .replace("&", "§") + args);
                 return true;
             }
 
-            if ((args[0].equalsIgnoreCase(anunciar.getPrefixSubComandoPedirAjuda())) && (sender.hasPermission(anunciar.getPermUser()))) {
+            if ((args[0].equalsIgnoreCase(getPrefixSubComandoPedirAjuda())) && (sender.hasPermission(getPermUser()))) {
                 Bukkit.broadcastMessage("");
                 Bukkit.broadcastMessage("§c§l[!] §cAlguém está criando um pedido de emergência... Capturamos os dados!");
-                Bukkit.broadcastMessage("§c§l[!] " + ChatColor.RED + anunciar.getJogadorLocalizationThis(sender));
+                Bukkit.broadcastMessage("§c§l[!] " + ChatColor.RED + getJogadorLocalizationThis(sender));
                 Bukkit.broadcastMessage("");
                 return true;
             }
         }
-        if ((sender.hasPermission(anunciar.getPermUser()) || (sender.hasPermission(anunciar.getPermVIP())) || (sender.hasPermission(anunciar.getPermStreamer()) || (sender.hasPermission(anunciar.getPermAdministrador()))))){
-            sender.sendMessage(anunciar.getCmdIncompleto());
+        if ((sender.hasPermission(getPermUser()) || (sender.hasPermission(getPermVIP())) || (sender.hasPermission(getPermStreamer()) || (sender.hasPermission(getPermAdministrador()))))){
+            sender.sendMessage(getCmdIncompleto());
             return false;
         }
 
-        sender.sendMessage(anunciar.getCmdSemPermissao());
+        sender.sendMessage(getCmdSemPermissao());
 
         return false;
 
